ff9ae4472e794dc44354c1e6b33406c9d81d602f
aravadikesh <aravadikesh@gmail.com>
2025-06-11 21:14:33 -0400

    initial edits to cds template

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
-project = "cdstemplate"
+project = "flexa"
 copyright = "2023, UMass Amherst Center for Data Science"
 author = "Luke Ruud"
 
