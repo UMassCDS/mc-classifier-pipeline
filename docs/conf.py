b86f8ba606928c1aecbe840d5adcfb63634d4ba3
aravadikesh <aravadikesh@gmail.com>
2025-06-23 13:07:32 -0400

    changed package name to generic one

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
-project = "flexa"
+project = "mc_classifier_pipeline"
 copyright = "2023, UMass Amherst Center for Data Science"
 author = "Luke Ruud"
 
