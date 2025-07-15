cc7dbcfaa7788f9427c39d836a8b546b19ba4244
hdrahman <haamed1.450@gmail.com>
2025-07-15 09:51:57 -0400

    ruff format

--- src/mc_classifier_pipeline/doc_retriever.py
+++ src/mc_classifier_pipeline/doc_retriever.py
@@ -143,7 +143,7 @@ def search_mediacloud_by_query(
         actual_end = end_date if end_date is not None else default_end
         if collection_ids:
             logger.info(f"Query restricted to {len(collection_ids)}(s) collections with ids: {collection_ids}")
-            results = SEARCH_API.story_list(query, actual_start, actual_end, collection_ids= collection_ids)
+            results = SEARCH_API.story_list(query, actual_start, actual_end, collection_ids=collection_ids)
         else:
             results = SEARCH_API.story_list(query, actual_start, actual_end)
         if results and len(results[0]) > 0:
@@ -355,7 +355,12 @@ def parse_arguments():
 
     parser.add_argument("--end-date", required=True, type=str, help="End date for query search (YYYY-MM-DD format)")
 
-    parser.add_argument("--collection-ids",nargs="*",type=int,help=("List of collection IDs to limit the search to (ID1 ID2 ... format)"),)
+    parser.add_argument(
+        "--collection-ids",
+        nargs="*",
+        type=int,
+        help=("List of collection IDs to limit the search to (ID1 ID2 ... format)"),
+    )
 
     # json formatted for label studio
     parser.add_argument(
