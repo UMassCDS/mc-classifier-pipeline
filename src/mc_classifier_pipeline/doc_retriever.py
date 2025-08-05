002fddb7785e7af9d5c2205139a942ed80eaf3f1
aravadikesh <aravadikesh@gmail.com>
2025-08-05 15:39:20 -0400

    strEnum was not avaiable in 3.10, so replaced with regular enum

--- src/mc_classifier_pipeline/doc_retriever.py
+++ src/mc_classifier_pipeline/doc_retriever.py
@@ -4,7 +4,7 @@ import json
 import logging
 import os
 from collections import Counter
-from enum import StrEnum
+from enum import Enum
 from pathlib import Path
 from typing import Optional
 
@@ -20,7 +20,7 @@ utils.configure_logging()
 logger = logging.getLogger(__name__)
 
 
-class ArticleStatus(StrEnum):
+class ArticleStatus(str, Enum):
     SUCCESS = "success"
     FAILED_NO_TEXT = "failed_no_text"
     UNKNOWN = "unknown"
